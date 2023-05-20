32a3d79a372665d89756fe77e24e1d8c05b9daa0
sanjeev.bl97 <sanjeev.bl97@gmail.com>
2023-05-20 11:46:44 +0530

    Done

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -10,7 +10,9 @@ public class Student {
     private int age;
     private double averageScore;
 
-    public Student(){
-
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
     }
 }
